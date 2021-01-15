f0511782a10c1e5a62ed221138bdd404d1749efc
Eric Liu <ericyl.us@gmail.com>
2021-01-15 05:26:44 -0800

    test: import module from lib

--- src/Test.svelte
+++ src/Test.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
-  import { AddDevice } from "../types";
-  import ActiveServer from "../types/ActiveServer";
-  import BerlinTower from "../types/BerlinTower";
+  import { AddDevice } from "../lib";
+  import ActiveServer from "../lib/ActiveServer.svelte";
+  import BerlinTower from "../lib/BerlinTower.svelte";
 </script>
 
 <AddDevice aria-label="Add Device" />
 <ActiveServer class="class" style="fill: red" title="" tabindex="0" />
-<BerlinTower fill="" />
+<BerlinTower fill="#000" />
