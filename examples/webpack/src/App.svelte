53069fa53ccd134fd7e5ed55645446e6cdf8fb69
metonym <ericyl.us@gmail.com>
2021-09-24 12:41:56 -0700

    chore: remove old docs, examples

--- examples/webpack/src/App.svelte
+++
@@ -1,7 +0,0 @@
-<script>
-  import { ActiveServer } from "carbon-pictograms-svelte";
-  import Airplane from "carbon-pictograms-svelte/lib/Airplane.svelte";
-</script>
-
-<ActiveServer fill="blue" width="96" height="96" />
-<Airplane />
