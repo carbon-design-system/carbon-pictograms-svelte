033800fc2054a8114aa0d64332365b4efc773187
Eric Liu <ericyl.us@gmail.com>
2023-07-08 12:39:22 -0700

    chore(preview): upgrade deps [ci skip]

--- preview/vite.config.js
+++ preview/vite.config.js
@@ -2,6 +2,9 @@ import { sveltekit } from "@sveltejs/kit/vite";
 
 /** @type {import('vite').UserConfig} */
 const config = {
+  optimizeDeps: {
+    exclude: ["carbon-components-svelte", "carbon-icons-svelte"],
+  },
   plugins: [sveltekit()],
 };
 
