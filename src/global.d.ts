0635ea909f546ebbe1a92c57f42db55d444154ff
Eric Liu <ericyl.us@gmail.com>
2024-08-18 11:03:34 -0700

    refactor: use type instead of interface

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,24 +1,22 @@
 declare module "@carbon/pictograms" {
   import { toString } from "@carbon/icon-helpers";
 
-  interface PictogramAttrs {
-    xmlns: "http://www.w3.org/2000/svg";
-    viewBox: "0 0 32 32";
-    fill: "currentColor";
-    width: 64;
-    height: 64;
-  }
-
-  export interface PictogramOutput {
+  type PictogramOutput = {
     moduleName: string;
     filepath: string;
     descriptor: {
       elem: "svg";
-      attrs: PictogramAttrs;
+      attrs: {
+        xmlns: "http://www.w3.org/2000/svg";
+        viewBox: "0 0 32 32";
+        fill: "currentColor";
+        width: 64;
+        height: 64;
+      };
       content: Parameters<typeof toString>[0][];
       name: string;
     };
-  }
+  };
 
   export type BuildIcons = {
     icons: ReadonlyArray<{
