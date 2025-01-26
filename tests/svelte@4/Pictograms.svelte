36e54dbc0efd152a6f21808c7e6cbe401f340bd8
Eric Liu <ericyl.us@gmail.com>
2025-01-26 12:11:01 -0800

    feat(types)!: use `Component` type for type definitions

--- tests/svelte@4/Pictograms.svelte
+++ tests/svelte@4/Pictograms.svelte
@@ -10,13 +10,26 @@
   import Airplane from "carbon-pictograms-svelte/lib/Airplane.svelte";
   import BerlinTower from "carbon-pictograms-svelte/lib/BerlinTower.svelte";
   import type { CarbonPictogramProps } from "carbon-pictograms-svelte";
+  import { mount } from "svelte";
 
-  const props: CarbonPictogramProps = {
+   // Use the exported `CarbonPictogramProps` type for typing objects since the 
+  // idiomatic `ComponentProps` utility is not compatible with `Component` in Svelte 4.
+  const props= {
     fill: "red",
-  };
-  const pictogram = new Airplane({ target: document.body, props });
-  $: console.log(pictogram.$$prop_def);
-  $: console.log(typeof Airplane);
+  } satisfies CarbonPictogramProps;
+
+  // Instead, you can use a const assertion for type safety.
+  const component_props = {
+    fill: "red",
+    "data-test": "id",
+  } as const;
+
+  // @ts-expect-error (Svelte components are no longer classes)
+  // See https://svelte.dev/docs/svelte/v5-migration-guide#Components-are-no-longer-classes
+  const icon_class = new Airplane({ target: document.body, props });
+  
+  // Instead, use the `mount` function to manually instantiate the component.
+  mount(Airplane, { target: document.body, props: component_props });
 
   const component = AssetManagement;
 </script>
@@ -25,13 +38,11 @@
 <ActiveServer class="class" style="fill: red" title="" tabindex={0} />
 <BerlinTower fill="#000" />
 
-<!-- svelte-ignore reactive-component -->
 <Airplane aria-label="Airplane" />
 
 <!-- svelte-ignore a11y-label-has-associated-control -->
 <label id="transportation">Transportation</label>
 
-<!-- svelte-ignore reactive-component -->
 <Airplane aria-labelledby="transportation" />
 
 <AiEthics />
--- tests/svelte@5/Pictograms.svelte
+++ tests/svelte@5/Pictograms.svelte
@@ -11,13 +11,26 @@
   import BerlinTower from "carbon-pictograms-svelte/lib/BerlinTower.svelte";
   import type { CarbonPictogramProps } from "carbon-pictograms-svelte";
   import { mount } from "svelte";
+  import type { ComponentProps } from "svelte";
 
-  const props: CarbonPictogramProps = {
+   // Use the exported `CarbonPictogramProps` type for typing objects since the 
+  // idiomatic `ComponentProps` utility is not compatible with `Component` in Svelte 4.
+  const props= {
     fill: "red",
-  };
-  const pictogram = mount(Airplane, { target: document.body, props });
-  $: console.log(pictogram.$$prop_def);
-  $: console.log(typeof Airplane);
+  } satisfies CarbonPictogramProps;
+
+  // Or, you can use the idiomatic `ComponentProps` utility.
+  const component_props = {
+    fill: "red",
+    "data-test": "id",
+  } satisfies ComponentProps<typeof Airplane>;
+
+  // @ts-expect-error (Svelte components are no longer classes)
+  // See https://svelte.dev/docs/svelte/v5-migration-guide#Components-are-no-longer-classes
+  const icon_class = new Airplane({ target: document.body, props });
+  
+  // Instead, use the `mount` function to manually instantiate the component.
+  mount(Airplane, { target: document.body, props: component_props });
 
   const component = AssetManagement;
 </script>
