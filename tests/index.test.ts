88f08b2a6dfbbd163d52b5bd31daa99b9ccce3fe
Eric Liu <ericyl.us@gmail.com>
2025-12-18 19:33:45 -0800

    feat(deps-dev): upgrade `@carbon/pictograms` 12.65.0 --> 12.66.0 (#114)

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -5,7 +5,7 @@ import { template } from "../src/template";
 
 test("imports", async () => {
   const pictograms = await buildPictograms();
-  expect(pictograms.length).toEqual(1517);
+  expect(pictograms.length).toEqual(1520);
   expect(pictograms).toMatchSnapshot();
 }, 30_000);
 
